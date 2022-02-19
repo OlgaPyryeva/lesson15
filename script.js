5666a75354012fed0215ab51f661745b6ff43127
Olga Pyryeva <vashkevicholga@gmail.com>
2022-02-20 00:13:35 +0300

    fix(script.js): fix cod lesson15

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = "";
-  this.height = "";
-  this.width = "";
-  this.bg = "";
-  this.fontSize = "";
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.backgroundColor = bg;
+  this.fontSize = fontSize;
 };
 
-DomElement.prototype.createElemMethod = function () {
-  const elem = document.querySelectorAll(".elements");
-  elem.forEach(function (item) {
-    const elemText = item.textContent.split("", 1);
-    const firstDiv = document.getElementById("div1");
+DomElement.prototype.createElemMethod = function (text) {
+  if (this.selector[0] === ".") {
+    const newDiv = document.createElement("div");
+    newDiv.className = this.selector.slice(1);
+    newDiv.textContent = text;
+    newDiv.style.cssText = `
+       height:${this.height}px;
+       width:${this.width}px;
+       background-color:${this.backgroundColor};
+       font-size:${this.fontSize}px`;
 
-    if (elemText[0] == ".") {
-      const newDiv = document.createElement("div");
-      newDiv.className = ".block";
-      newDiv.textContent = "новый блок";
+    document.body.prepend(newDiv);
+  }
+  if (this.selector[0] === "#") {
+    const newP = document.createElement("p");
+    newP.id = "second";
+    newP.textContent = "новый параграф";
+    newP.style.cssText = `
+       height:${this.height}px;
+       width:${this.width}px;
+       background-color:${this.backgroundColor};
+       font-size:${this.fontSize}px`;
 
-      document.body.before(newDiv, firstDiv);
-    } else if (elemText[0] == "#") {
-      const newP = document.createElement("p");
-      newP.id = "second";
-      newP.textContent = "новый параграф";
-      document.body.before(newP, firstDiv);
-    }
-  });
+    document.body.prepend(newP);
+  }
 };
-const domElem = new DomElement();
-domElem.createElemMethod();
+
+const domElem = new DomElement(".block", 10, 10, "red", 10);
+domElem.createElemMethod("текст");
